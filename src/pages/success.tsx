ea183e5f2c4e3b69baf7cbe7defd2f9308aeb87a
DIOGO-MOTA <diogomota21@gmail.com>
2022-09-11 12:46:41 -0300

    page success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success(){
   return (
-    <h1>teste</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Diogo Mota</strong>, sua <strong>Camisa beyoud the Limits</strong>
+      </p>
+
+      <Link href='/'>
+      Voltar ao Catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
